567ba9e1908e77aeef9e284dad4882777edd7af6
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2025-01-28 20:29:32 +0100

    added menu bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,12 @@ android {
 
 dependencies {
 
-    implementation(libs.appcompat)
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.12.0")
 }
